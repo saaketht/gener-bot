1734e2cd482aa970abd441fc77edf032f1215d14
saaketht <sakethtallam@yahoo.com>
2022-02-18 07:37:38 -0500

    env3

--- message-events/weather.js
+++ message-events/weather.js
@@ -1,11 +1,11 @@
 module.exports = {
 	name: 'weather',
-	async execute(message) {
+	execute(message) {
 		if (message.author.bot) return;
 		const command = message.content.split(' ').join('').toLowerCase();
 		if (command.includes('weather')) {
 			console.log(command.split(' '));
-			await message.reply('pong');
+			message.reply('pong');
 		}
 	},
 };
