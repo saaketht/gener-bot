0367a1516a8e3ec60e3837ba4bb282a41b8d41dd
saaketht <sakethtallam@yahoo.com>
2022-02-18 05:10:02 -0500

    commit message

--- message-events/imagesearch.js
+++ message-events/imagesearch.js
@@ -1,5 +1,6 @@
 // api via rapidapi
 const axios = require('axios').default;
+require('dotenv').config()
 const { xRapidApiKey } = process.env.rapidApiKey; // require('./config.json');
 const searchCommand = 'imagesearch';
 module.exports = {
