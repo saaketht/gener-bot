6b16ddc78afdb5bcea67833282050a3b96be2014
saaketht <sakethtallam@yahoo.com>
2022-02-23 21:52:43 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -32,7 +32,7 @@ module.exports = {
 			if (searchQuery != '') {
 				console.log('search query: ' + searchQuery.join('+'));
 				const response = await openai.createCompletion('text-davinci-001', {
-					prompt: searchQuery.join(' '),
+					prompt: 'Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: ' + searchQuery.join(' ') + '\nMarv:',
 					temperature: 0.6,
 					max_tokens: 150,
 					top_p: 0.3,
