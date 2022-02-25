b7d4259e45ebb8dbd4800acb99183fbe59e3a89f
saaketht <sakethtallam@yahoo.com>
2022-02-24 23:42:07 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -33,7 +33,7 @@ module.exports = {
 				console.log('search query: ' + searchQuery.join('+'));
 				const response = await openai.createCompletion('text-davinci-001', {
 					// prompt: 'generBot is an edgelord chatbot who responds well to loaded questions with depth but with a hint of sarcasm:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					prompt: 'generBot is a bangladeshi male in college named rateeb riyasat:\n\nYou: Where do you live jit\ngenerBot: 5300 kim court bruh\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					prompt: 'generBot is a bangladeshi male in college named rateeb riyasat:\n\nYou: Where do you live jit\ngenerBot: 5300 kim court bruh\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
 					temperature: 0.5,
 					max_tokens: 150,
 					top_p: 0.3,
