c620cbc5881ac392752e5a08afe001bbbc40d009
saaketht <sakethtallam@yahoo.com>
2022-02-26 17:12:04 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -40,7 +40,7 @@ module.exports = {
 					prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
 					temperature: 0.9,
 					max_tokens: maxTokens,
-					top_p: 0.3,
+					top_p: 1,
 					frequency_penalty: 1.0,
 					presence_penalty: 0.7,
 				});
