fcb79d81af238962e224796e9c5abc18ac4552c2
saaketht <sakethtallam@yahoo.com>
2022-02-26 16:06:22 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -40,7 +40,7 @@ module.exports = {
 					frequency_penalty: 2.0,
 					presence_penalty: 0.7,
 				});
-				console.log(response.data.choices[0]);
+				console.log(typeof response.data.choices[0]);
 				const completion = response.data.choices[0];
 				message.reply(completion);
 			}
