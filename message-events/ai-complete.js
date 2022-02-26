e50f29f684dfcaf979a2ad8082529c7f9f182939
saaketht <sakethtallam@yahoo.com>
2022-02-26 17:34:58 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -36,8 +36,8 @@ module.exports = {
 			if (searchQuery != '') {
 				console.log('search query: ' + searchQuery.join('+'));
 				const response = await openai.createCompletion('text-curie-001', {
-					// prompt: 'generBot is an edgelord chatbot who responds well to loaded questions with depth but with a hint of sarcasm:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					prompt: 'generBot is an edgelord chatbot who responds well to loaded questions with depth but with a hint of sarcasm:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					// prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
 					temperature: 0.9,
 					max_tokens: maxTokens,
 					top_p: 1,
