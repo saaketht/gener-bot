c0159afb22f64c002dd545837f469683c9953f5d
saaketht <sakethtallam@yahoo.com>
2022-03-01 14:04:38 -0500

    massive speed improvements

--- message-events/interruptions.js
+++ message-events/interruptions.js
@@ -3,35 +3,35 @@ const fetch = require('node-fetch');
 
 module.exports = {
 	name: 'interruptions',
-	execute(message) {
+	async execute(message) {
 		if (message.author.bot) return;
 		const command = message.content.split(' ').join('').toLowerCase();
 		if (command.includes('bruh')) {
 			console.log(command.split(' '));
-			message.reply('bruh');
+			await message.reply('bruh');
 		}
 		else if (command.includes('crazy')) {
 			console.log(command.split(' '));
 			const x = randomIntFromInterval(1, 3);
 			if (x == 1) {
-				message.reply('for real');
+				await message.reply('for real');
 			}
 			else if (x == 2) {
-				message.reply('ong');
+				await message.reply('ong');
 			}
 			else {
-				message.reply('fr');
+				await message.reply('fr');
 			}
 		}
 		else if (command.includes('cat')) {
 			console.log(command.split(' '));
 			const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 			console.log(file);
-			message.reply(file);
+			await message.reply(file);
 		}
 		else if (command.includes('ping')) {
 			console.log(command.split(' '));
-			message.reply('pong');
+			await message.reply('pong');
 		}
 	},
 };
