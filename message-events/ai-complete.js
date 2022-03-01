974e6e0009b622d77d4de499ca64dddc019b2c2b
saaketht <sakethtallam@yahoo.com>
2022-02-28 19:26:39 -0500

    small change

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -38,7 +38,7 @@ module.exports = {
 				const response = await openai.createCompletion('text-curie-001', {
 					// prompt: 'generBot is a philosophical chatbot that is somewhat nihilistic and standoffish but responds well to loaded questions with depth and a hint of sarcasm:\n\n' + message.author.username + ' ' + searchQuery.join(' ') + '\ngenerBot:',
 					// prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					prompt: 'generBot is playboi carti, the rapper:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					prompt: 'generBot is travis scott:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
 					temperature: 0.9,
 					max_tokens: maxTokens,
 					top_p: 1,
