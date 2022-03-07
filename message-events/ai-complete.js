18d2fff7e890229052c6df41c06d072ea679a215
saaketht <sakethtallam@yahoo.com>
2022-03-07 01:00:52 -0500

    changes

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -20,8 +20,10 @@ module.exports = {
 				return;
 			} */
 			let maxTokens = 100;
+			let modelName = 'curie';
 			if (message.author.username.includes('gener')) {
 				maxTokens = 1800;
+				modelName = 'davinci'
 			}
 			console.log('author: ' + message.author.username + ', command: ' + command);
 			const configuration = new Configuration({
@@ -35,17 +37,17 @@ module.exports = {
 			}
 			if (searchQuery != '') {
 				console.log('search query: ' + searchQuery.join('+'));
-				const response = await openai.createCompletion('text-curie-001', {
+				const response = await openai.createCompletion('text-' + modelName + '-001', {
 					// prompt: 'generBot is a philosophical chatbot that is somewhat nihilistic and standoffish but responds well to loaded questions with depth and a hint of sarcasm:\n\n' + message.author.username + ' ' + searchQuery.join(' ') + '\ngenerBot:',
-					prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					// prompt: 'generBot is travis scott:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					temperature: 0.9,
+					// prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					prompt: 'generBot is a chatbot that is quite knowledgeable about a variety of subjects:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					temperature: 0.5,
 					max_tokens: maxTokens,
-					top_p: 1,
-					frequency_penalty: 1.0,
-					presence_penalty: 0.7,
+					top_p: 0.3,
+					frequency_penalty: 0.5,
+					presence_penalty: 0.0,
 				});
-				console.log('AI RESPONSE: ' + typeof response.data.choices[0].text);
+				console.log('RESPONSE TYPE: ' + typeof response.data.choices[0].text);
 				const completion = response.data.choices[0].text;
 				console.log(completion);
 				message.reply(completion);
