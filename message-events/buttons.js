6fcfec2ea0ec268a5c5b3976d4c55b80787be3b7
saaketht <sakethtallam@yahoo.com>
2022-03-07 05:00:08 -0500

    changes

--- message-events/buttons.js
+++ message-events/buttons.js
@@ -7,26 +7,27 @@ module.exports = {
         if (message.author.bot) return;
         const command = message.content.toLowerCase().split(' ');
         const searchIndex = command.findIndex(checkIndex);
+		if (command.includes('buttons')) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Some title')
+                .setURL('https://discord.js.org/')
+                .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+                .setDescription('Some description here')
+                .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+                .addFields(
+                    { name: 'Regular field title', value: 'Some value here' },
+                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Inline field title', value: 'Some value here', inline: true },
+                    { name: 'Inline field title', value: 'Some value here', inline: true },
+                )
+                .addField('Inline field title', 'Some value here', true)
+                .setImage('https://i.imgur.com/AfFp7pu.png')
+                .setTimestamp()
+                .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Some title')
-            .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-            .setDescription('Some description here')
-            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            .addField('Inline field title', 'Some value here', true)
-            .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-        message.reply({ embeds: [exampleEmbed] });
+            message.reply({ embeds: [exampleEmbed] });
+        }
     },
 };
 
