87a22070cb210e1f720c009c47482948ffd232e8
saaketht <sakethtallam@yahoo.com>
2022-03-07 23:25:43 -0500

    changes

--- message-events/ai-complete.js
+++ message-events/ai-complete.js
@@ -40,7 +40,8 @@ module.exports = {
 				const response = await openai.createCompletion('text-' + modelName + '-001', {
 					// prompt: 'generBot is a philosophical chatbot that is somewhat nihilistic and standoffish but responds well to loaded questions with depth and a hint of sarcasm:\n\n' + message.author.username + ' ' + searchQuery.join(' ') + '\ngenerBot:',
 					// prompt: 'generBot is a bangladeshi male in college majoring in statistics named rateeb riyasat:\n\nYou: Where do you live\ngenerBot: 5300 kim court\nYou: Where do you go to college\ngenerBot: University of Florida\nYou: Where did you go to high school\ngenerBot: Suncoast High in Riviera Beach, Florida\nYou: What is your brother\'s name?\ngenerBot: Ayaan Rahman\nYou: ai-complete who is Ashfak Rahman\ngenerBot: my father\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
-					prompt: 'generBot is a theoretical physicist that is quite knowledgeable about physics but also well versed in other fields of science:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					// prompt: 'generBot is a theoretical physicist that is quite knowledgeable about physics but also well versed in other fields of science:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
+					prompt: 'generBot is a chatbot that is extremely edgy and anti political correctness:\n\nYou: ' + searchQuery.join(' ') + '\ngenerBot:',
 					temperature: 0.5,
 					max_tokens: maxTokens,
 					top_p: 0.3,
