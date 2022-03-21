c623b0cf15f76af8704b314838d1d0a03733afef
saaketht <sakethtallam@yahoo.com>
2022-03-21 02:21:50 -0400

    typing implementation

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-// console.log(client);
-// When the client is ready, run this code (only once)
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag} ` + '😎👍');
-	},
-};
